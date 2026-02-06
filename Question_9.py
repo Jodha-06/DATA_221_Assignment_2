c880933ca489a292cfe371af042bf90d38b9eee7
Jodha-06 <Jodhveerd06@gmail.com>
2026-02-06 16:46:38 -0700

    Add comments to Question_9.py for improved clarity and readability

--- Question_9.py
+++ Question_9.py
@@ -1,28 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-
+#Set a User-Agent so Wikipedia allows the request
 headersInOrderToAccessWikipedia = {"User-Agent": "Mozilla/5.0"}
+#Download HTML content of the wikipedia page
 retrieveHtmlPage = requests.get("https://en.wikipedia.org/wiki/Machine_learning",headers=headersInOrderToAccessWikipedia).text
 soup = BeautifulSoup(retrieveHtmlPage, "html5lib")
 
+#Find the main article content
 mainContentDiv = soup.find("div", id="mw-content-text")
+#Find all tables in the main content
 allTables = mainContentDiv.find_all("table")
 
 selectedTable = None
 
+#Choose first table with at least 3 data rows
 for table in allTables:
-    tableRows = table.find_all("tr")
-    if len(tableRows) >= 4:
+    dataRows = table.find_all("tr")
+    numberOfDataRows = 0
+
+    for row in dataRows:
+        if row.find_all("td"):
+            numberOfDataRows += 1
+
+    if numberOfDataRows >= 3:
         selectedTable = table
         break
 
+#Stop if no table is found and output a message
 if selectedTable is None:
     print("No suitable table found.")
     exit()
 
 allRows = selectedTable.find_all("tr")
 
+#list of table data
 listofTableData = []
 
 for row in allRows:
@@ -35,9 +47,11 @@ for row in allRows:
     if rowValues:
         listofTableData.append(rowValues)
 
+#Decide table headers
 headerRow = listofTableData[0]
 dataRows = listofTableData[1:]
 
+#Check if the headers exist
 headersExist = True
 for header in headerRow:
     if header == "":
@@ -51,6 +65,7 @@ else:
     for i in range(len(headerRow)):
         headers.append(f"col{i + 1}")
 
+#Make sure thta all rows have the same number of columns
 maxNumberOfColumns = len(headers)
 
 for row in dataRows:
@@ -59,6 +74,7 @@ for row in dataRows:
     while len(row) > maxNumberOfColumns:
         row.pop()
 
+#Save all data to a new file named "wiki_table.csv"
 with open("wiki_table.csv", "w", newline="", encoding="utf-8") as csvFile:
     csvWriter = csv.writer(csvFile)
     csvWriter.writerow(headers)
