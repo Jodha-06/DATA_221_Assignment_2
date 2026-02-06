5e4ecbdcf3778c4a83f26334979e0e211b803d58
Jodha-06 <Jodhveerd06@gmail.com>
2026-02-06 16:24:07 -0700

    Add comments to Question_7.py for improved clarity and readability

--- Question_8.py
+++ Question_8.py
@@ -1,20 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
+#Set a User-Agent so Wikipedia allows the request
 headersInOrderToAccessWikipedia = {'User-Agent': 'Mozilla/5.0'}
+#Download HTML content of the wikipedia page
 retrieveHtmlPage = requests.get("https://en.wikipedia.org/wiki/Data_science",headers=headersInOrderToAccessWikipedia).text
 
 soup = BeautifulSoup(retrieveHtmlPage, "html5lib")
 
+#Find the main article content
 contentDiv = soup.find("div", id="mw-content-text")
 
+#list to store all valid section headings
 listofHeadings = []
 
+#Loop through every "h2" tag in the main content
 for h2 in contentDiv.find_all("h2"):
+    #Strip all whitescape from the text
     headingText = h2.get_text().strip()
 
+    #Remove the "edit" text
     headingText = headingText.replace("[edit]","").strip()
 
+    #list of headings that are not valid
     unwantedHeadings = ["References", "External links", "See also", "Notes"]
     skippedHeading = False
 
@@ -27,6 +35,7 @@ for h2 in contentDiv.find_all("h2"):
 
     listofHeadings.append(headingText)
 
+#Save all valid headings to a new text file called "headings.txt"
 with open("headings.txt","w",encoding="utf-8") as file:
     for heading in listofHeadings:
         file.write(heading+"\n")
